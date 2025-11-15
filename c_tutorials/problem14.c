5a80080fe1bd180ab5fb15b05b2964d4ba6482aa
tarikk45 <mtarikkb@gmail.com>
2025-11-15 19:56:18 +0300

    s

--- c_tutorials/problem14.c
+++ c_tutorials/problem14.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int operationSelection();
 int resultCalculator(int num);
 
 int main(){
-    
+    while (1){
     printf("Menu\n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - Modulus\n6 - Exponentiation\n7 - Exit\nPlease select the operation.\n");
     int numm = operationSelection();
+    if (numm == 7){
+        printf("END");
+        return 0;
+    }
     int resultt = resultCalculator(numm);
+    
     printf("The result is %d",resultt);
+    printf("\n\n");
+     
+    }
     return 0;
 }
 
@@ -40,6 +48,7 @@ int resultCalculator(int num){
     else if (num == 4){
         if (num2 == 0){
             printf("Invalid");
+            exit(0);
         }
         else {
             result = num1 / num2;
@@ -48,6 +57,7 @@ int resultCalculator(int num){
     else if (num == 5){
         if (num2 == 0){
             printf("Invalid");
+            exit(0);
         }
         else {
             result = num1 % num2;
@@ -59,5 +69,7 @@ int resultCalculator(int num){
             result *= num1;
         }
     }
+    
+    
     return result;
 }
